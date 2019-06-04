f5ee8b61c11c9cd359e00313d6987c5671f16a72
Piotr <piteropeter@gmail.com>
2019-06-05 00:45:16 +0200

    Pre login user

--- src/main/java/com/example/demo/ui/controller/UserController.java
+++ src/main/java/com/example/demo/ui/controller/UserController.java
@@ -1,24 +1,11 @@
 package com.example.demo.ui.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserService;
@@ -27,7 +14,7 @@ import com.example.demo.ui.model.request.UserDetailsRequestModel;
 import com.example.demo.ui.model.response.UserRest;
 
 @RestController
-@RequestMapping("users") // http://localhost:8080/users
+@RequestMapping("") // http://localhost:8080/users
 public class UserController {
 	@Autowired
 	UserService userService;
@@ -51,16 +38,6 @@ public class UserController {
 		
 		return returnValue;
 	}
-	
-	@PutMapping
-	public String updateUser() {
-		return "update user was called";
-	}
-	
-	@DeleteMapping
-	public String deleteUser() {
-		return "delete user was called";
-	}
 
 	@GetMapping({"/", "/index"})
     public String index() {
@@ -70,12 +47,12 @@ public class UserController {
 		  "<a href=\"registration\">Zarejestruj sie</a>.</p></div>"+
 		  "</div> <div class=\"container signin\"><p>Masz konto? " + 
 		  "<a href=\"login\">Logowanie</a>.</p></div>";
-
 	}
+	
 	@GetMapping("/login")
     public String login() {
 
-    	String data = "<form action=\"login\" method=\"get\" target=\"_blank\">" + 
+    	String data = "<form>" + 
   		"Email: <input type=\"text\" name=\"email\"><br>" +
   		"Password: <input type=\"password\" name=\"password\"><br>"+
   		"<button type=\"submit\" formmethod=\"post\">Zatwierdź</button></form>" +
@@ -88,8 +65,8 @@ public class UserController {
 	@PostMapping("/login")
 	public String login(@RequestBody String dane) {
 		String[] arguments = dane.split("&");
-		String email = arguments[0];
-		String password = arguments[1]; 
+		String email = arguments[0].split("=")[1];
+		String password = arguments[0].split("=")[1]; 
 
 
 		return dane;
@@ -97,7 +74,7 @@ public class UserController {
 
 	@GetMapping("/registration")
     public String registration() {
-		String data = "<form action=\"login\" method=\"get\" target=\"_blank\">" + 
+		String data = "<form>" + 
 		"<br><label for=\"firstName\"><b>First Name</b></label>"+ 
 		"<input type=\"text\" placeholder=\"Enter Your first name\" name=\"firstName\" required>"+	
 		"<br><label for=\"lastName\"><b>Last Name</b></label>"+ 
@@ -107,7 +84,7 @@ public class UserController {
 		"<br><label for=\"psw\"><b>Password</b></label>" +
 		"<input type=\"password\" placeholder=\"Enter Password\" name=\"psw\" required>" +
 		"<br><label for=\"psw-repeat\"><b>Repeat Password</b></label>" + 
-		"<input type=\"password\" placeholder=\"Repeat Password\" name=\"psw-repeat\" required>"+
+		"<input type=\"password\" placeholder=\"Repeat Password\" name=\"psw_repeat\" required>"+
   		"<button type=\"submit\" formmethod=\"post\">Zatwierdź</button></form>" +
 		"</div> <div class=\"container signin\"><p>Masz już konto? " + 
 		"<a href=\"login\">Zaloguj się</a>.</p></div>";
@@ -118,6 +95,11 @@ public class UserController {
 	@PostMapping("/registration")
 	public String registration(@RequestBody String dane) {
 		String[] arguments = dane.split("&");
+		String firstName = arguments[0].split("=")[1];
+		String lastName = arguments[1].split("=")[1]; 
+		String email = arguments[2].split("=")[1]; 
+		String psw = arguments[3].split("=")[1]; 
+		String psw_repeat = arguments[4].split("=")[1]; 
 
 		return dane;
 	}
